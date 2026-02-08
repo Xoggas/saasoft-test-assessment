7307f2e0b1c35f957058d781642d2126908f6295
Xoggas <sashakor16@gmail.com>
2026-02-08 12:37:54 +0300

    Changed theme

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import Lara from '@primeuix/themes/lara';
 
 const app = createApp(App);
-app.use(PrimeVue);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara
+    }
+});
+
 app.mount('#app');
