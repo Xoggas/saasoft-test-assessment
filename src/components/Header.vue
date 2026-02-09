402e9cbfbed1fcfb92bb99038d0f1136f895ab90
Xoggas <sashakor16@gmail.com>
2026-02-09 12:57:39 +0300

    Implemented header component

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {useAccountStore} from "../stores/useAccountStore.ts";
+
+const {create: createUser} = useAccountStore();
+
+</script>
+
+<template>
+  <header>
+    <h3>Учетные записи</h3>
+    <Button
+      @click="createUser"
+      icon="pi pi-plus"
+      variant="outlined"/>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+}
+</style>
