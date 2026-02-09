6f22d378b58a5daac1105225985a80e54763737f
Xoggas <sashakor16@gmail.com>
2026-02-09 12:57:49 +0300

    Implemented Hint component

---
+++ src/components/Hint.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hint">
+    <i class="pi pi-question-circle" style="font-size: 1.5rem"></i>
+    <span>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</span>
+  </div>
+</template>
+
+<style scoped>
+.hint {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 0.3rem 0.5rem;
+  background-color: var(--p-primary-200);
+  border-radius: 0.2rem;
+}
+</style>
